3fb568bcd5128371bb7be8e4e448c71712d11c05
manan-bedi2908 <mananbedilps@gmail.com>
2024-04-07 20:16:28 +0530

    zero shot prompting enabled

--- src/mcq_generator/MCQGenerator.py
+++ src/mcq_generator/MCQGenerator.py
@@ -16,3 +16,25 @@ load_dotenv()
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 llm = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.8)
+
+template="""
+Text: {text}
+You are an expert MCQ Maker. Given the above text, it is your job to \
+create a quiz of {number} multiple choice questions for {subject} students in {tone} tone.
+Make sure the questions are not repeated and check all the questions to be conforming the text as well.
+Make sure to format your response like RESPONSE_JSON below and use it as a guide. \
+Ensure to make {number} MCQs
+
+{response_json}
+"""
+
+quiz_generation_prompt = PromptTemplate(
+    input_variables=['text', 'number', 'subject', 'tone', 'response_json'],
+    template=template
+)
+
+quiz_chain = LLMChain(llm = llm,
+                      prompt = quiz_generation_prompt,
+                      output_key='quiz',
+                      verbose = True)
+
