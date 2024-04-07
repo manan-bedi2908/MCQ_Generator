9ffd134ada601bd76a37e24d12f0bfe89d66e7b6
manan-bedi2908 <mananbedilps@gmail.com>
2024-04-07 20:22:42 +0530

    setup utils.py

--- src/mcq_generator/MCQGenerator.py
+++ src/mcq_generator/MCQGenerator.py
@@ -38,3 +38,29 @@ quiz_chain = LLMChain(llm = llm,
                       output_key='quiz',
                       verbose = True)
 
+template2="""
+You are an expert english grammarian and writer. Given a Multiple Choice Quiz for {subject} students.\
+You need to evaluate the complexity of the question and give a complete analysis of the quiz. Only use at max 50 words for complexity analysis. 
+if the quiz is not at per with the cognitive and analytical abilities of the students,\
+update the quiz questions which needs to be changed and change the tone such that it perfectly fits the student abilities
+Quiz_MCQs:
+{quiz}
+
+Check from an expert English Writer of the above quiz:
+"""
+
+quiz_evaluation_prompt = PromptTemplate(
+    input_variables=['subject', 'quiz'],
+    template=template2
+)
+
+review_chain=LLMChain(llm=llm, prompt=quiz_evaluation_prompt, 
+                      output_key="review", verbose=True)
+
+generate_evaluate_chain=SequentialChain(chains=[quiz_chain, review_chain], 
+                                        input_variables=["text", "number", "subject", "tone", "response_json"],
+                                        output_variables=["quiz", "review"], 
+                                        verbose=True)
+
+file_path = r'C:\Users\Manan Bedi\Documents\LLM_iNeuron\MCQ_Generator\data.txt'
+
